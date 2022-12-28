cddb2e766bd8ede97ed8e89ebdc9cff5f9cb3867
Hernando <lzhen.dev@outlook.com>
2022-12-28 14:39:33 +0800

    configured 模糊音

---
+++ scripts/install_clover.sh
@@ -0,0 +1,12 @@
+# #!/bin/bash
+# ###
+#  # @Date: 2022-12-28 13:56:01
+#  # @LastEditors: Hernando lzhen.dev@outlook.com
+#  # @LastEditTime: 2022-12-28 14:00:52
+#  # @FilePath: \Rime\scripts\install_clover.sh
+# ### 
+# # download cloverpinyin
+# curl https://github.com/fkxxyz/rime-cloverpinyin/releases/download/1.1.4/clover.schema-1.1.4.zip
+
+# # unzip cloverpinyin
+# unzip clover.schema-1.1.4.zip
